e084693759cdb720bac400e4487a5b4f1121bf2e
Thomas Chabaud <tchabaud@users.noreply.github.com>
2015-04-27 10:13:36 +0200

    Create choco.bat

---
+++ choco.bat
@@ -0,0 +1,1 @@
+choco install 7zip chocolateygui flashplayerplugin apachedirectorystudio vim winmerge wireshark intellijidea-community webstorm nodejs ccleaner vmwareplayer filezilla google-chrome-x64 svn sysinternals fiddler opera windirstat
